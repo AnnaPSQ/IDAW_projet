08d9e81ff60bb6326a3530f63372dacafc3058dc
AnnaPSQ <annapasquier4@gmail.com>
2021-03-29 10:48:59 +0200

    commit

---
+++ frontend/aliments.php
@@ -0,0 +1,8 @@
+<?php
+
+//page affichant les aliments de la base avec la possibilité d’en ajouter, d’en supprimer,
+//de les modifier  : Penser à la PAGINATION
+
+
+//relier avec notre bdd et particulièrement la table aliments
+?>
---
+++ frontend/profil.php
@@ -0,0 +1,3 @@
+<?php
+//Page qui permet de renseigner nos informations (Voir utilisateurs database)
+?>
