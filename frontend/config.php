fdc411b9ec2851b6ee6895ca7e3e0f2d851ce4c0
Antoine GORON <antoine.goron@etu.imt-lille-douai.fr>
2021-03-29 14:44:00 +0200

    bonjouer

--- frontend/config.php
+++ frontend/config.php
@@ -107,18 +107,3 @@
         </script>
             </div>
         </div>        
-
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Cours PHP / MySQL</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="cours.css">
-    </head>
-    <body>
-        <h1>Bases de données MySQL</h1>  
-        <?php require_once('')
-        ?>
-    </body>
-</html>
