a62baf446aa07bb1f1c973a6e00b3d76afdc6b31
Antoine GORON <antoine.goron@etu.imt-lille-douai.fr>
2021-03-29 14:45:16 +0200

    nouveau

--- frontend/header.php
+++ frontend/header.php
@@ -4,6 +4,8 @@
         <title>Nourrissez-vous bien!</title>
         <link rel="stylesheet" href="<?php echo $currentStyle; ?>.css" type="text/css"
         media="screen" title="default" charset="utf-8" />
+        <meta charset="utf-8">
+        <script src = "https://code.jquery.com/jquery-3.6.0.js"></script>
     </head>
     <body>
         <header class="bandeau_haut">
