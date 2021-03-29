d4a2a8d6f13765a1afd5908ae1d03e1872585e38
Antoine GORON <antoine.goron@etu.imt-lille-douai.fr>
2021-03-29 11:33:36 +0200

    xxsq

---
+++ backend/config.php
@@ -0,0 +1,13 @@
+<?php
+            $servername = 'localhost';
+            $username = 'root';
+            $password = '';
+            
+            //On établit la connexion
+            $conn = new mysqli($servername, $username, $password);
+            
+            //On vérifie la connexion
+            if($conn->connect_error){
+                die('Erreur : ' .$conn->connect_error);
+            }            
+?>
