50ead89939c003866a5845d8a283a54e1ad97b15
Antoine GORON <antoine.goron@etu.imt-lille-douai.fr>
2021-03-31 21:34:56 +0200

    fin Jalon 1

--- backend/addAliment.php
+++ backend/addAliment.php
@@ -1,28 +1,29 @@
 <?php
             $servname = "localhost"; $dbname = "projetidaw"; $user = "root"; $pass = "";
+            print_r($_POST);
             try{
                 $dbco = new PDO("mysql:host=$servname;dbname=$dbname;charset=utf8", $user, $pass);
                 $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                $query1 = "INSERT INTO aliments (Nom, Type) VALUES('$_POST[nom]', '$_POST[type]')";
+                $query1 = "INSERT INTO aliments (ID_aliments, Nom, Type) VALUES('$_POST[id]', '$_POST[nom]', '$_POST[type]')";
                 $dbco -> exec($query1);
                 
 
                 $query2 =
                 "INSERT INTO a_pour_apport(ID_apport, ID_aliments, Ratio)
                 VALUES
-                (1,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[energie]'), 
-                (3,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[proteines]'),
-                (4,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[glucides]'), 
-                (5,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[lipides]'), 
-                (6,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[sucres]'), 
-                (7,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[cholesterol]'), 
-                (8,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[calcium]'), 
-                (9,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[fer]'),
-                (10,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[magnesium]'), 
-                (11,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[phosphore]'),
-                (12,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[potassium]'), 
-                (13,(SELECT ID_aliments FROM aliments WHERE nom='$_POST[nom]'),'$_POST[sodium]');  
+                (1,'$_POST[id]','$_POST[energie]'), 
+                (3,'$_POST[id]','$_POST[proteines]'),
+                (4,'$_POST[id]','$_POST[glucides]'), 
+                (5,'$_POST[id]','$_POST[lipides]'), 
+                (6,'$_POST[id]','$_POST[sucres]'), 
+                (7,'$_POST[id]','$_POST[cholesterol]'), 
+                (8,'$_POST[id]','$_POST[calcium]'), 
+                (9,'$_POST[id]','$_POST[fer]'),
+                (10,'$_POST[id]','$_POST[magnesium]'), 
+                (11,'$_POST[id]','$_POST[phosphore]'),
+                (12,'$_POST[id]','$_POST[potassium]'), 
+                (13,'$_POST[id]','$_POST[sodium]');  
                 ";
                 
                 $dbco -> exec($query2);
