4d609e5c63cd604d6df9675609721be3714e2ef9
AnnaPSQ <annapasquier4@gmail.com>
2021-04-03 21:09:20 +0200

    commit

--- frontend/journal.php
+++ frontend/journal.php
@@ -32,42 +32,6 @@
             
             let repas = [];
             let urlBackendPrefix = "http://localhost/IDAW_projet/IDAW_projet/backend/";          
-
-            // function AjaxEnvoieInfosVoulues(infos){
-            //     $.ajax({
-            //         //L'URL de la requête 
-            //         url: urlBackendPrefix+"afficheRepas.php",
-
-            //         //La méthode d'envoi (type de requête)
-            //         method: "POST",
-
-            //         //Le format de réponse attendu
-            //         dataType : "json",
-            //         data : infos,
-            //         success : function(response){
-
-            //         // .always(function(response){
-            //                 console.log('success');
-            //                 repas = response;
-            //                 $.each(repas, function(i,r){
-            //                     let unRepas = {};
-            //                     unRepas.id_repas = r.ID_repas;
-            //                     unRepas.date = r.Date;
-            //                     unRepas.type_repas = r.Type_repas;
-            //                     unRepas.type_aliment = r.Type;
-            //                     unRepas.nom_aliment = r.Nom;
-            //                     unRepas.quantite = r.Quantite;
-            //                     // console.log(unRepas);
-            //                     ajouteRepasTable(unRepas);
-            //                 });
-            //         // })
-            //         },
-            //         error : function(data, statut, error){
-            //             console.log(error);
-            //             console.log('erreur');
-            //         }
-            //     });
-            // };
             
             function AjaxAfficheRepas(login){
                 
@@ -103,17 +67,8 @@
             function onFormSubmit(){
                 event.preventDefault();
 
-                // let infos_voulues = {};
-                // infos_voulues.login = $("#IDlogin").val() ;
-                // infos_voulues.date = $("#IDdate").val() ;
-
                 let login_value = $("input[name='login']").val();
                 AjaxAfficheRepas(login_value);
-
-                // if (infos_voulues.login !='' ){
-                    
-                //     AjaxAfficheRepas(infos_voulues.login);
-                // }  
             }
 
             function ajouteRepasTable(repasAd){
