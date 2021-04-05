8ad8ea4860b0bc55e393081ca4a80181a9ac1265
AnnaPSQ <annapasquier4@gmail.com>
2021-04-06 01:20:00 +0200

    commit

--- backend/config.php
+++ backend/config.php
@@ -1,1 +1,4 @@
-Pour utiliser ce code, il faut changer les url (des urlBackendPrefix sont souvent mis en début de code et doivent être changés).
+<?php
+
+echo"Pour utiliser ce code, il faut changer les url (des urlBackendPrefix sont souvent mis en début de code et doivent être changés)."
+echo"On pourra tester les affichages pour l'utilisateur 'anna.pasquier@etu.imt-lille-douai.fr' pour qui la base de données a été particulièrement renseignée."
